1e3484b9e4526ed9cc94195ce936c438f2d90332
gharjare <gharjarechetan@gmail.com>
2023-01-29 14:49:33 +0530

    [chetan gharjare] used switch cases

--- Program.cs
+++ Program.cs
@@ -7,29 +7,32 @@ namespace EmployeeWagesProblem
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to EmployeeWage Computation problem");
-            int IS_FULL_TIME = 1;
+            const int IS_FULL_TIME = 1;
             int Emp_Rate_Per_Hour = 20;
-            int IS_PART_TIME = 2;
+           const int IS_PART_TIME = 2;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0,2);
-            if(empCheck == IS_FULL_TIME)
+            switch(empCheck)
             {
-                Console.WriteLine("Employee is present");
-                empHrs = 8;
-                empWage = empHrs * Emp_Rate_Per_Hour;
+                case IS_FULL_TIME:
+                    Console.WriteLine("Employee is full time");
+                    empHrs = 8;
+                    empWage = empHrs * Emp_Rate_Per_Hour;
+                    break;
+
+                case IS_PART_TIME:
+                    Console.WriteLine("Employee is part time ");
+                    empHrs = 4;
+                    break;
+
+                default:
+                    Console.WriteLine("Employee is absent");
+                    break;
             }
-            else if(empCheck == IS_PART_TIME)
-            {
-                empHrs = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is absent");
-                empHrs = 0;
-            }
-            Console.WriteLine("EmployeeWage" + empWage);
+            Console.WriteLine("Emp Wage : " + empWage);
+
         }
     }
 }
