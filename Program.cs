685b7a355a4f33a2ebaf2ad3e52feb612b6d5d91
gharjare <gharjarechetan@gmail.com>
2023-01-29 14:31:37 +0530

    [chetan gharjare] Here we check employee is present or absent

--- Program.cs
+++ Program.cs
@@ -1,11 +1,23 @@
-﻿namespace EmployeeWagesProblem
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace EmployeeWagesProblem
 {
     class program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to EmployeeWage Computation problem");
-
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0,2);
+            if(empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
         }
     }
 }
